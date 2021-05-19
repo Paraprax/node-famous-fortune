21b40401be5a233ab32ef64eb74b31a697a37b6b
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-05-18 20:54:50 -0400

    added random-number-generator variables for picking phrases from arrays

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const phraseArray1 = ["Starring"];
+const phraseArray1 = ["Starring", "barring", "zarring", "marring", "pharing"];
 const phraseArray2 = ["In a popular"];
 const phraseArray3 = [
   "on live television",
@@ -12,6 +12,14 @@ const phraseArray3 = [
 const fortuneMessage = [];
 
 const messageBuilder = (array1, array2, array3) => {
+  let index1 = Math.floor(Math.random() * [array1.length + 1]);
+  let index2 = Math.floor(Math.random() * [array2.length + 1]);
+  let index3 = Math.floor(Math.random() * [array2.length + 1]);
+
+  console.log(index1);
+  console.log(index2);
+  console.log(index3);
+
   let message = `${array1[0]} ${array2[0]} ${array3[0]}!`;
   return message;
 };
