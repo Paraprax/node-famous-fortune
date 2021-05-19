12f9f7cffe24603fe1bef2f9fa6b67979f7b6e41
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-05-18 21:01:16 -0400

    comments

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//one phrase from each of these arrays will be used to make up the fortune message:
 const phraseArray1 = ["Starring", "barring", "zarring", "marring", "pharing"];
 const phraseArray2 = ["In a popular"];
 const phraseArray3 = [
@@ -11,12 +12,15 @@ const phraseArray3 = [
 
 const fortuneMessage = [];
 
+//function for generating a random index number within the length of any given array:
 const indexGenerator = (array) => {
   let randomIndex = Math.floor(Math.random() * [array.length + 1]);
   return randomIndex;
 };
 
+//function for assembling && returning the fortune message from the three arrays of phrases:
 const messageBuilder = (array1, array2, array3) => {
+  //generate a random index number for each phrase array:
   let index1 = indexGenerator(array1);
   let index2 = indexGenerator(array2);
   let index3 = indexGenerator(array3);
